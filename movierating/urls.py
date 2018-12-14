dd977deba48bca06d205ff96a08d4b95014363f4
sunche <sunche@umich.edu>
2018-12-13 20:58:45 -0500

    Finish

--- movierating/urls.py
+++ movierating/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
    path('', views.HomePageView.as_view(), name='home'),
-   path('about/', views.AboutPageView.as_view(), name='about'),
    path('movies/', views.MovieListView.as_view(), name='movies'),
    path('movies/<int:pk>/', views.MovieDetailView.as_view(), name='movie_detail'),
    path('movies/new/', views.MovieCreateView.as_view(), name='movie_new'),
